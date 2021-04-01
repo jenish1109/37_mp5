4a1fb55dc148eb537581abe41a81848d25312ab3
Jenish Patel <180420116037.it18@scet.ac.in>
2021-04-01 23:42:21 +0530

    commit

--- ArmstrongClient.js
+++ ArmstrongClient.js
@@ -8,6 +8,7 @@ client.on('message', function(num, info) {
     console.log('Data received from server : ' + num.toString());
     //console.log('Received %d bytes from %s:%d\n', str.length, info.address, info.port);
 });
+
 //sending msg
 client.send(data, 2222, 'localhost', function(error) {
     if (error) {
